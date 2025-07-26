4827732a0275f093cfe9b83dc6bce1cdbe0aab09
monissheikh1234 <158022523+monissheikh1234@users.noreply.github.com>
2025-07-26 14:38:29 +0530

    Update index.js added frontend url from render

--- server/index.js
+++ server/index.js
@@ -17,7 +17,15 @@ app.get("/", (req, res) => {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://codesynced.vercel.app"],
+    origin: [
+      "http://localhost:5173", // for local dev
+      "https://syncscriptio-client.onrender.com" // for production
+    ],
+    methods: ["GET", "POST"],
+  },
+});
+
+"],
     methods: ["GET", "POST"],
   },
 });
